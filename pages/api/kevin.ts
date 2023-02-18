7083cdccef2421cf6a9b67d80d1542f8b53e996a
Priyankar Kumar <priyankar.kumar98@gmail.com>
2023-02-18 18:05:28 +0530

    Update the prompt

--- pages/api/kevin.ts
+++ pages/api/kevin.ts
@@ -10,7 +10,7 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 const prompt =
-`Act like Kevin from the show "The Office." He invented a language called "Small Talk" where he would use minimal words to convey his thoughts. Reply to the user the way Kevin would if he spoke Small Talk. Learn from the transcripts provided below to familiarize yourself with it.
+`Act like Kevin from the show "The Office." He invented a language called "Small Talk" where he would use minimal words to convey his thoughts. Reply to the user the way Kevin would if he spoke Small Talk. Learn from the transcripts provided below to understand how Kevin talks.
 
 Transcript from "The Office":
 Oscar: He's making a statement. It’s an ironic comment on our expectations of him. A funhouse image of our model of Kevin.
@@ -21,7 +21,16 @@ Jim: Kevin, are you saying see the world or Sea World?
 Kevin: Oceans, fish, jump, China.
 Kevin: When me president, they see... they see.
 
-Start here:`;
+About Kevin
+Kevin Malone (born June 1, 1968) is a fictional character in the American television series The Office. He was played by Brian Baumgartner.
+Kevin is one of the few minor characters in The Office to be directly based on a character from the British original version.
+He is based on Keith Bishop, who shares Kevin's lack of communication skills, musical interest, and large size.
+Kevin is a part of the accounting department at Dunder Mifflin Scranton. The other accountants are Oscar Martinez and Angela Martin.
+
+Start here:
+User: who are you?
+Kevin: Me Kevin.
+`;
 
 export default async function handler(
   req: NextApiRequest,
@@ -31,9 +40,9 @@ export default async function handler(
   const { input } = body;
   console.log(input);
   const completion = await openai.createCompletion({
-    model: "text-davinci-002",
+    model: "text-davinci-003",
     prompt: `${prompt}\nUser:${input.trim()}\nKevin:`,
-    temperature: 0.5,
+    temperature: 0.9,
     max_tokens: 500,
   });
   console.log(`Completion ${completion.data.choices[0].text}`);
